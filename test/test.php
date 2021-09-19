1c35471c6000c4e73b64d2d54124e104376fee9d
nguyenanhung <info@nguyenanhung.com>
2021-09-20 06:12:22 +0700

    Add GitHub Action Workflows

---
+++ test/test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Project spreadsheets-basic
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 09/20/2021
+ * Time: 06:02
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use nguyenanhung\Google\Basic\Spreadsheets\GoogleSpreadsheets;
+
+$sheet = new GoogleSpreadsheets();
+
